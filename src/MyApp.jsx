85866b826ea8714b84e30bd0919d900c033fa4ac
Ezequiel <ezequielrebasa@gmail.com>
2021-06-05 14:24:38 -0300

    navBar and TabsBar

---
+++ src/MyApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './Components/NavBar'
+import TabsBar from './Components/TabsBar'
+
+const MyApp = () => {
+    return (
+        <>
+            <NavBar />
+            <TabsBar />
+        </>
+    )
+}
+
+export default MyApp
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import MyApp from './MyApp';
+
+
+
+const divRoot = document.querySelector('#root');
+
+ReactDOM.render( <MyApp />, divRoot);
+
